ab637e067989e1940fe813ad19a37b0339cc7048
Riyanshi Agrawal <riyanshiagrawal2001@gmail.com>
2022-02-22 21:48:18 +0530

    Models added

---
+++ Leave_Mngmt/leave/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import employee,leave
+
+
+admin.site.register(leave)
+admin.site.register(employee)
+# Register your models here.
---
+++ Leave_Mngmt/leave/models.py
@@ -0,0 +1,40 @@
+
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+
+class employee(models.Model):
+    Manager='Manager'
+    Employee='Employee'
+    
+    Position=(
+        (Manager,'Manager'),
+        (Employee,'Employee'),
+    )
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    name=models.CharField(max_length=200,null=False,blank=False)
+    profile=models.CharField(max_length=200,default=Employee,choices=Position)
+    past_leaves=models.IntegerField(validators=[MinValueValidator(0),
+                                       MaxValueValidator(10)],default=0)
+
+    def __str__(self):
+        return name
+    
+class leave(models.Model):
+     name=models.CharField(max_length=200,null=False,blank=False)
+     designation=models.CharField(max_length=200,null=False,blank=False)
+     reason=models.CharField(max_length=1000,null=False,blank=False)
+     start_date=models.DateTimeField(null=False,blank=False,default=timezone.now)
+     end_date=models.DateTimeField(null=False,blank=False)
+     
+     def __str__(self):
+         return reason
+
+    
+
+
+
